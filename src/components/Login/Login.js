712bfa63552973ec6d3318151cb07328dcfd5136
Rupesh <rupesh.kumar@ilogsolution.com>
2023-02-21 14:39:03 +0530

    Pull Done

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,16 +1,16 @@
 import './login.scss'
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Icon from '../../images/awllogo.png'
 import { UserLogin } from '../../api/index'
 import { FaUser } from 'react-icons/fa';
 import { RiLoginCircleFill } from 'react-icons/ri';
-import { GoEye,GoEyeClosed } from 'react-icons/go';
+import { GoEye, GoEyeClosed } from 'react-icons/go';
 
 function Login() {
-    const [showerror,setShowerror]= useState(false);
-    const [showpass,setShowpass] = useState(false);
+    const [showerror, setShowerror] = useState(false);
+    const [showpass, setShowpass] = useState(false);
 
-    const toggleicon =()=>{
+    const toggleicon = () => {
         setShowpass(!showpass);
     }
     const handleClick = async (e) => {
@@ -18,16 +18,14 @@ function Login() {
         const username = document.getElementById('username').value
         const password = document.getElementById('password').value
         const result = await UserLogin(username, password)
-        console.log(result)
         if (result.result === username.toUpperCase()) {
-            console.log("Hello")
             localStorage.setItem('userId', result.result)
             localStorage.setItem('userName', result.result2)
             localStorage.setItem('Warehouse', result.result3)
             localStorage.setItem('warehouseId', result.result4.trim())
             window.location.href = '/Dashboard';
         }
-        else{
+        else {
             setShowerror(true);
         }
 
@@ -42,31 +40,31 @@ function Login() {
                             <div className="d-flex justify-content-center">
                                 <div className="brand_logo_container">
                                     <img src={Icon} className="brand_logo" alt="Logo" />
-                                </div>  
+                                </div>
                             </div>
                             <div className="d-flex justify-content-center form_container">
                                 <form>
-                                    <h4 className='mb-4 text-center' style={{color:"white"}}>LOG IN
-                                    {/* <RiLoginCircleFill style={{margin:"0 0 4px 5px"}}/> */}
+                                    <h4 className='mb-4 text-center' style={{ color: "white" }}>LOG IN
+                                        {/* <RiLoginCircleFill style={{margin:"0 0 4px 5px"}}/> */}
                                     </h4>
-                                    <div className="input-group mb-3" style={{borderBottom:"3px solid white"}}>
-                                        <FaUser style={{color:"white",margin:"15px 6px 0 0",fontSize:"20px"}} />
-                                        <input type="text" className='input' autocomplete="off" id="username" placeholder="Username" onChange={()=>{setShowerror(false)}}/>
+                                    <div className="input-group mb-3" style={{ borderBottom: "3px solid white" }}>
+                                        <FaUser style={{ color: "white", margin: "15px 6px 0 0", fontSize: "20px" }} />
+                                        <input type="text" className='input' autoComplete="off" id="username" placeholder="Username" onChange={() => { setShowerror(false) }} />
                                     </div>
-                                    <div className="input-group mb-2" style={{borderBottom:"3px solid white"}}>
+                                    <div className="input-group mb-2" style={{ borderBottom: "3px solid white" }}>
                                         {
-                                        showpass?
-                                        <GoEye onClick={toggleicon} style={{color:"white",margin:"15px 6px 0 0",fontSize:"20px"}}/>
-                                        :<GoEyeClosed onClick={toggleicon} style={{color:"white",margin:"15px 6px 0 0",fontSize:"20px"}}/>}
-                                        <input type={showpass?"text":"password"} className='input'  id="password" placeholder="Password" onChange={()=>{setShowerror(false)}} />
+                                            showpass ?
+                                                <GoEye onClick={toggleicon} style={{ color: "white", margin: "15px 6px 0 0", fontSize: "20px" }} />
+                                                : <GoEyeClosed onClick={toggleicon} style={{ color: "white", margin: "15px 6px 0 0", fontSize: "20px" }} />}
+                                        <input type={showpass ? "text" : "password"} className='input' id="password" placeholder="Password" onChange={() => { setShowerror(false) }} />
                                         <div className="input-group-append" >
 
                                         </div>
                                     </div>
-                                     {
-                                        showerror?
-                                        <><p style={{color:"red"}}>Please! enter valid username and Password </p></>:null
-                                     }
+                                    {
+                                        showerror ?
+                                            <><p style={{ color: "red" }}>Please! enter valid username and Password </p></> : null
+                                    }
                                     <div className="d-flex justify-content-center mt-4 my-4 login_container">
                                         <button type="button" onClick={handleClick} className="btn btn-secondary btn-block mt-3">Login</button>
 
